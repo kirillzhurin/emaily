b8a6cc3e71c2623af27edc2cdbf5a9c5207a9552
Kirill Zhurin <kirillzhurin@gmail.com>
2019-02-14 21:24:26 +0300

    init emaily

---
+++ client/src/components/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { fetchUser } from '../actions';
+import Header from './Header';
+import Landing from './Landing';
+
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+
+class App extends React.Component {
+  
+  componentDidMount() {
+    this.props.fetchUser();    
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route path="/" exact component={Landing} />
+            <Route path="/surveys" exact component={Dashboard} />
+            <Route path="/surveys/new" exact component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default connect(null, { fetchUser })(App);
---
+++ client/src/components/Payments.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import StripeCheckout from 'react-stripe-checkout';
+import { handleToken } from '../actions';
+
+class Payments extends React.Component {
+
+  render() {
+    return (
+      <StripeCheckout 
+        name="Emaily" 
+        description="$5 for 5 email credits"
+        amount={500} 
+        token={this.props.handleToken} 
+        stripeKey={process.env.REACT_APP_STRIPE_KEY}>
+        <button className="btn">
+          Add Credits
+        </button>
+      </StripeCheckout>
+    );
+  }
+};
+
+export default connect(null, { handleToken })(Payments);
